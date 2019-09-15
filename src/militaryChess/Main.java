adc529f8e2dc1e44db4962806696b74a29fa7e27
Diode222 <544481521@qq.com>
2019-09-16 01:05:30 +0800

    fix boom when last movable chess

--- src/militaryChess/Main.java
+++ src/militaryChess/Main.java
@@ -1,10 +1,14 @@
 package militaryChess;
 
+import chessPostionInfo.Position;
 import global.BoardLayout;
+import mcts.CallLocation;
 import mcts.FinalSelectionPolicy;
 import mcts.MCTS;
 import mcts.Move;
+import utils.ListUDG;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
@@ -12,32 +16,12 @@ public class Main {
     static public void main(String[] args) {
 //        // TODO test
 //        ListUDG udg = ListUDG.getUdg();
-////        int[][] board =  {
-//////                {1, 0, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}, // x = 0
-//////                {13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 50}, // x = 1
-//////                {0, 26, 27, 28, 29, 30, 31, 32, 33, 34, 46, 36}, // x = 2
-//////                {35, 24, 39, 40, 41, 42, 0, 44, 47, 45, 0, 37}, // x = 3
-//////                {49, 25, 0, 0, 0, 0, 2, 0, 0, 0, 38, 0}, // x = 4
-//////                {50 ,0 ,0, 0, 0 ,0 ,31, 0 ,6, 0 ,20 ,0},
-//////                {26, 49 ,0, 0, 0, 0 ,16 ,0, 0, 5 ,10 ,17},
-//////                {48 ,0, 0 ,0, 0, 0, 0 ,0 ,0 ,0 ,11, 23},
-//////                {42, 0, 0, 30, 0 ,0, 0 ,0 ,0 ,0 ,24, 1},
-//////                {0 ,45 ,0, 27, 0 ,0 ,0 ,0, 0 ,0, 0 ,25}
-////                {50   ,  0    , 0    , 0    , 0    , 0    , 0     ,0   ,  0   ,  0   ,  0   ,  0},
-////
-////                {26   , 22   ,  0   ,  0    , 0 ,    0  ,   0 ,    0    , 0  ,   0  ,   0  ,  17},
-////
-////                {48  ,   0   ,  0    , 0  ,   0    , 0    , 0    , 0    , 0   ,  0  ,  11  ,  23},
-////
-////                {42   ,  0    , 0   ,  0   ,  0 ,    0  ,   0   ,  0    , 0    , 0  ,  24   ,  1},
-////
-////                {45  ,   8     ,0   ,  0    , 0,     0   ,  0   ,  0   ,  0    , 0   ,  0   , 25}
-////        };
 //        int[][] board = new BoardLayout().getTest_layout();
 //
-//        JunQiBoard junQiBoard = new JunQiBoard();
+//        MilitaryBoard junQiBoard = new MilitaryBoard();
 //        junQiBoard.initBoard(board);
 //        junQiBoard.currentPlayer = 1;
+//        junQiBoard.backHandRemainMovableChessNum = 1;
 //
 //        ArrayList<Move> moves = new ArrayList<>();
 //
@@ -45,7 +29,7 @@ public class Main {
 //        moves = junQiBoard.getMoves(CallLocation.treePolicy);
 //
 //        for (Move move: moves) {
-//            JunQiMove junQiMove = (JunQiMove) move;
+//            MilitaryMove junQiMove = (MilitaryMove) move;
 //            for (Position position: junQiMove.points) {
 //                System.out.println(position.getX() + " " + position.getY());
 //            }
@@ -56,7 +40,7 @@ public class Main {
 //        junQiBoard.bPrint();
 
         MCTS mcts = new MCTS();
-        mcts.setExplorationConstant(0.2d); // TODO 可调节参数，越大代表越倾向于找访问次数较少的节点，会降低搜索深度提高宽度
+        mcts.setExplorationConstant(10.0d); // TODO 可调节参数，越大代表越倾向于找访问次数较少的节点，会降低搜索深度提高宽度
         mcts.setTimeDisplay(true);
         Move move;
         mcts.setOptimisticBias(0.0d);
